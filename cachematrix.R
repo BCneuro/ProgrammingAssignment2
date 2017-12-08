9a53532c6b60b18f55144c904106395534e678e0
BCneuro <33439318+BCneuro@users.noreply.github.com>
2017-12-08 15:58:24 -0500

    Completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Together, these two functions provide the ability to invert a matrix,
+## cache the inverted matrix, check whether there is already a value
+## cached, and calculate a new one if there is not.
 
-## Write a short comment describing this function
+## The first function creates two objects.  The first is x, a function
+## that remains to be defined.  The second is invmat, where the 
+## inverse matrix can be stored.  The get and set functions define where
+## the values/functions that populate the objects will come from, and 
+## allows previous values to be overwritten.
 
 makeCacheMatrix <- function(x = matrix()) {
+  invmat <- NULL
+  set <- function(y) {
+    x <<- y
+    invmat <<- NULL
+  }
+  get <- function() x
+  setinv <- function(invert) invmat <<- invert
+  getinv <- function() invmat
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 
 }
 
 
-## Write a short comment describing this function
+## This function checks to see whether there is already a cached matrix 
+## available for invmat.  If there is, it prints "retrieving cached data"
+## and retrieves the cahced matrix.  If not, it uses the solve function
+## to invert the matrix, and prints and caches this new matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invmat <- x$getinv()
+  if(!is.null(invmat)) {
+    message("retrieving cached data")
+    return(invmat)
+  }
+  data <- x$get()
+  invmat <- solve(data)
+  x$setinv(invmat)
+  invmat
 }
+
+
+
